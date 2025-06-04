c6324c3125bf95b67dd8bf5fe95f4884d9617d48
Lucy <solouser019@gmail.com>
2025-06-04 16:41:07 +0530

    code is not working properly so remove the code and again start

--- script.js
+++ script.js
@@ -1,87 +1,19 @@
 function openFeatures() {
-  let elem = document.querySelectorAll(".elem");
-  let allfullElem = document.querySelectorAll(".fullElem");
-  let backbtn = document.querySelectorAll(".fullElem .backbtn");
-  elem.forEach(function (elem) {
+  var allElems = document.querySelectorAll(".elem");
+  var fullElemPage = document.querySelectorAll(".fullElem");
+  var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
+
+  allElems.forEach(function (elem) {
     elem.addEventListener("click", function () {
-      allfullElem[elem.id].style.display = "block";
+      fullElemPage[elem.id].style.display = "block";
     });
   });
 
-  backbtn.forEach(function (back) {
+  fullElemPageBackBtn.forEach(function (back) {
     back.addEventListener("click", function () {
-      allfullElem[back.id].style.display = "none";
+     back.closest('.fullElem').style.display = 'none';
     });
   });
 }
-
-// openFeatures();
-
-function todoList(){
+openFeatures();
  
-var currentTask = []
- 
-if(localStorage.getItem("currentTask")){
-  currentTask = JSON.parse(localStorage.getItem("currentTask"))
-}else{
-  console.log("Task is not empty")
-} 
-
-
-function renderTask() {
-    localStorage.setItem("currentTask",JSON.stringify (currentTask))
-  var allTask = document.querySelector(".allTask");
-  var sum = "";
-  currentTask.forEach(function (elem,index) {
-    sum =
-      sum +
-      `<div class="task"> 
-                <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-                <button id=${index}>Mark as Completed</button>
-            </div>`;
-  });
-  allTask.innerHTML = sum;
-  
-} 
-
-renderTask()
-
-let form = document.querySelector(".addTask form");
-let taskInput = document.querySelector(".addTask form #task-input");
-let taskDetailsInput = document.querySelector(".addTask form textarea ");
-let taskCheckBox = document.querySelector(".addTask form #check");
-
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  currentTask.push({
-    task: taskInput.value,
-    details: taskDetailsInput.value,
-    imp: taskCheckBox.checked,
-  });
-  
-  renderTask();
-  
-
-  taskInput.value = ''
-  taskDetailsInput.value = ''
-  taskCheckBox.value = false
-  renderTask()
-  location.reload()
-}); 
-
-
-
-var markCompletedBtn = document.querySelectorAll(".task button")     
-markCompletedBtn.forEach(function(button){
-  button.addEventListener("click",function(){
-     currentTask.splice(button.id,1) 
-     console.log("data")
-     renderTask()
-     location.reload()
-  })
-})
-}
-
-
-todoList()
